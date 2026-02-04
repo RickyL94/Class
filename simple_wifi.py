b9537c8dfa0dc12273422c21fb0b683e5f207533
RickyL94 <57995332+RickyL94@users.noreply.github.com>
2026-02-04 17:24:21 +0000

    Create simple_wifi.py

---
+++ simple_wifi.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.node import Controller
+
+def topology():
+    "Create a simple Wi-Fi network"
+    
+    # 1. Initialize the network
+    net = Mininet_wifi(controller=Controller)
+
+    info("*** Creating nodes\n")
+    # Add a generic controller
+    c0 = net.addController('c0')
+    
+    # Add an Access Point (mode 'g' = 802.11g)
+    ap1 = net.addAccessPoint('ap1', ssid='myssid', mode='g', channel='1', position='10,10,0')
+    
+    # Add a Station (Client)
+    sta1 = net.addStation('sta1', position='10,20,0')
+
+    info("*** Configuring nodes\n")
+    net.configureNodes()
+
+    info("*** Starting network\n")
+    net.build()
+    c0.start()
+    ap1.start([c0])
+
+    info("*** Running CLI\n")
+    # This gives you the 'mininet-wifi>' prompt to run commands manually
+    CLI(net)
+
+    info("*** Stopping network\n")
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
